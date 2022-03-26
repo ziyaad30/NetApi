f78a3244f25ff12b413c37870caaaf501e5cd3f5
Ziyaad30 <xavier.baatjes@outlook.com>
2022-03-26 14:18:21 +0200

    Initial commit

---
+++ netlify/src/main/java/com/xavier/netlify/NetApiFetch.java
@@ -0,0 +1,11 @@
+package com.xavier.netlify;
+
+import android.util.Log;
+
+public class NetApiFetch {
+    private final String TAG = NetApiFetch.class.getSimpleName();
+
+    private void showLog(String message) {
+        Log.e(TAG, message);
+    }
+}
