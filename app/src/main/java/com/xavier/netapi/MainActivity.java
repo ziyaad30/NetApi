4b5f2747b042480a72c5be9ba05b256f635259e2
Ziyaad30 <xavier.baatjes@outlook.com>
2022-03-26 19:48:38 +0200

    Update API to use OkHttp3

--- app/src/main/java/com/xavier/netapi/MainActivity.java
+++ app/src/main/java/com/xavier/netapi/MainActivity.java
@@ -1,8 +1,22 @@
 package com.xavier.netapi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
--- netlify/src/main/java/com/xavier/netlify/NetApiFetch.java
+++ netlify/src/main/java/com/xavier/netlify/NetApiFetch.java
@@ -2,12 +2,18 @@ package com.xavier.netlify;
 
 import android.util.Log;
 
-import com.androidnetworking.AndroidNetworking;
-import com.androidnetworking.error.ANError;
-import com.androidnetworking.interfaces.OkHttpResponseAndJSONObjectRequestListener;
+import androidx.annotation.NonNull;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.Response;
 
 public class NetApiFetch {
@@ -38,27 +44,48 @@ public class NetApiFetch {
     }
 
     private void fetchFromApi(){
-        AndroidNetworking.get(apiUrl)
-                .addHeaders("content-type", "application/json")
-                .build()
-                .getAsOkHttpResponseAndJSONObject(new OkHttpResponseAndJSONObjectRequestListener() {
-                    @Override
-                    public void onResponse(Response okHttpResponse, JSONObject jsonObject) {
-                        if (okHttpResponse.code() == 200) {
-                            listener.onSuccess(jsonObject);
-                            listener.onComplete("Netlify fetch, found "
-                                    + jsonObject.length() +
-                                    " results.");
-                        } else {
-                            listener.onFailed("Failed with " + okHttpResponse.message());
-                        }
-                    }
+        OkHttpClient client = new OkHttpClient();
 
-                    @Override
-                    public void onError(ANError anError) {
-                        listener.onFailed(anError.getMessage());
-                        showLog(" Netlify runner had errors " + anError.getLocalizedMessage());
-                    }
-                });
+        Request request = new Request.Builder()
+                .url(this.apiUrl)
+                .addHeader("content-type", "application/json")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                listener.onFailed(e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                Log.e(TAG, "Request executed: " + call.isExecuted());
+                if (response.code() != 200) {
+                    Log.e(TAG, "RESPONSE CODE: " + response.code());
+                    return;
+                }
+                if (response.isRedirect()) {
+                    Log.e(TAG, "REDIRECTED: " + response.isRedirect());
+                    return;
+                }
+                if (!response.isSuccessful()) {
+                    Log.e(TAG, "SUCCESS: " + response.isSuccessful());
+                    return;
+                }
+
+                try {
+                    JSONObject jsonObject = new JSONObject(Objects.requireNonNull(response.body()).string());
+                    Log.e(TAG, "NetApiFetch, found "
+                            + jsonObject.length() +
+                            " results.");
+                    Log.e(TAG, jsonObject.toString());
+                    listener.onSuccess(jsonObject);
+                } catch (JSONException e) {
+                    listener.onFailed(e.getMessage());
+                }
+                
+                listener.onComplete("NetApiFetch has completed!");
+            }
+        });
     }
 }
